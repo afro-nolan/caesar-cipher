99fef6fdad6bce464e65e8f17d906a7f82788dd5
Afro <aifric.nolan.gold@gmail.com>
2018-08-02 18:32:43 +0100

    Added Decrypt class

--- Decrypt.java
+++ Decrypt.java
@@ -3,19 +3,21 @@ import java.util.Map;
 import java.util.List;
 import java.util.LinkedList;
 
-public class Encrypt {
-	
-	private String message ;
+
+public class Decrypt {
+
+	private String message; 
 	private int n;
 
 	//constructor
-	public Encrypt(String message, int n) {
+	public  Decrypt(String message, int n) {
 		this.message = message;
-		this.n = n;
+		this.n  = n;
+
 	}
-	
-	public String encryptText() {
-	
+
+	public String decryptText() {
+
 		String lower = "abcedefghijklmnopqrstuvwxyz";
 		String upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
@@ -30,7 +32,7 @@ public class Encrypt {
 		String dst = lower.substring(n) + lower.substring(0, n) + upper.substring(n) + upper.substring(0, n);
 		String[] line1 = src.split("");
 		String[] line2 = dst.split("");
-	
+
 		Map<String, String> cipher = new HashMap<>() ;
 		
 
@@ -39,29 +41,24 @@ public class Encrypt {
 
 		}
 		List<String> output = new LinkedList<>();
-	
-		for (int i = 0; i < message.length(); i++) {
-			if (cipher.containsKey(message.substring(i,i+1))) {
-				output.add(cipher.get(message.substring(i,i+1)));
-			
-			}
-			else {
-				output.add(message.substring(i,i+1));
-			
-			}	
-		}
 
+		for (int i=0; i < message.length(); i++) {
+			for (Map.Entry<String, String> e : cipher.entrySet()) {
+				String key = e.getKey();
+				String value = e.getValue();
+
+				if (value.equals(message.substring(i, i+1))) {
+					output.add(key);
+				}
+			}
+		}
+		
 		String s = "";
 
 		for (String c : output) {
 			s += c;
-
 		}
-	
-		return s;
-		
-	}
 
-	
-	
+		return s;
+	}
 }
--- caesarController.java
+++ caesarController.java
@@ -18,6 +18,18 @@ public class caesarController {
 
     @FXML
     private TextField outputTextField;
+
+    @FXML
+    private TextField inputTextField1;
+
+    @FXML
+    private TextField outputTextField1;
+
+    @FXML
+    private Button decryptButton;
+
+    @FXML
+    private TextField numberTextField;
     
     public void initialize() {
     	
@@ -26,18 +38,55 @@ public class caesarController {
    @FXML
    private void encryptText(ActionEvent e) {
 	   //Get text from inputText Field
+	   String number = numberTextField.getText();
+     try {
+      int n = Integer.valueOf(number);
+
+      //Ensure text is not empty
+     if (inputTextField.getText() != null && inputTextField.getText().length() != 0) {
+     //Pass text to Encryption Class
+       String message = inputTextField.getText();
+       Encrypt hidden = new Encrypt(message, n);
+       String output = hidden.encryptText();
+     //return encrypted text and print to outputTExtField
+       outputTextField.setText(output);
+     }
+     }
+     catch (Exception exception) {
+      numberTextField.setText("Enter valid number");
+
+     }
 	   
-	   //Ensure text is not empty
-	   if (inputTextField.getText() != null && inputTextField.getText().length() != 0) {
-		 //Pass text to Encryption Class
-		   String message = inputTextField.getText();
-		   Encrypt hidden = new Encrypt(message);
-		   String output = hidden.encryptText();
-		 //return encrypted text and print to outputTExtField
-		   outputTextField.setText(output);
-	   }
-	   
-   }	
+   }
+
+   @FXML
+    void decryptText(ActionEvent event) {
+
+      //Get text from inputText Field
+     String number = numberTextField.getText();
+     try {
+      int n = Integer.parseInt(number);
+
+      if (inputTextField1.getText() != null && inputTextField1.getText().length() != 0) {
+
+        String message = inputTextField1.getText();
+        Decrypt hidden = new Decrypt(message, n);
+        String output = hidden.decryptText();
+
+        outputTextField1.setText(output);
+
+      }
+
+     }
+     catch (Exception exception) {
+      numberTextField.setText("Enter valid number");
+
+    }
+
+
+
+
+    }	
 
 }
 
